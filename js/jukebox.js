9503c53930e86c15ab8731e5e28828ba0e8c5859
github username <github email address>
2019-01-28 06:41:13 -0500

    Done.

--- js/jukebox.js
+++ js/jukebox.js
@@ -15,26 +15,26 @@ function randomNumber() {
 }
 
 function menu() {
-  $('#menu').on('click', function(e) {
+  $('#menu').on('click', function menu(e) {
     $('#menuOptions').html("You can play a song, pause a song, or view all the songs");
   });
 }
 
 function play() {
-  $('#play').on('click', function(e) {
+  $('#play').on('click', function play(e) {
     $('#songPlaying').html("now playing " + songToPlay + " by " + theArtist);
     return "now playing " + songToPlay + " by " + theArtist;
   });
 }
 
 function pause() {
-  $('#pause').on('click', function(e) {
+  $('#pause').on('click', function pause(e) {
     $('#songPaused').html(songToPlay + " is paused");
   });
 }
 
 function show() {
-  $('#show').on('click', function(e) {
+  $('#show').on('click', function show(e) {
     var songs = "";
     for (var key in jukeboxSongs) {
       songs += jukeboxSongs[key] + " by " + key +  " is available to play. "
